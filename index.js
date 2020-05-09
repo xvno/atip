7b935deaedd635dd553b6bd8e94c95290fb642ff
xvno <x@vno.io>
2020-05-09 17:55:56 +0800

    Mdf: log module

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const path = require('path')
+const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
-const PORT = process.env.PORT || 9000;
-const { log, warn } = console;
 const { unifyWidth } = require('./src/utils.js');
 
-app.use(async (ctx, next) => {
-    let startTS = Date.now();
-    await next();
-    log(`${unifyWidth(Date.now() - startTS)}ms: ${ctx.originalUrl}`);
-});
+// configure logger
+switch (process.env.NODE_ENV) {
+    case 'dev':
+        app.use(log);
+        break;
+    default:
+        app.use(require('koa-logger')());
+        break;
+}
 
 app.use(require('koa-static')(path.resolve(__dirname)));
-
 app.use((ctx) => {
-    ctx.body = 'Hello Koa';
+    ctx.body = 'Nothing here, check your url!';
 });
 
+async function log(ctx, next) {
+    let { request: req } = ctx;
+    let startTS = Date.now();
+    await next();
+    console.log(
+        `${unifyWidth(Date.now() - startTS)}ms: ${req.method} ${req.url}`
+    );
+}
+
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, function () {
     console.log(`Server started listen at port ${PORT}!`);
 });
